6eefc5a658e4531dc6769cf746006b1354731f0f
Alfred G.L Taylor <alfredgtaylor@gmail.com>
2024-06-12 14:54:28 +0100

    more updates to the database handling

--- src/meteo_brain/data/db.py
+++ src/meteo_brain/data/db.py
@@ -13,45 +13,64 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
-import typing
 import time
 import json
+
 
 class DBError(Exception):
     pass
 
+
+EMPTY_CONFIG = {
+    "date_of_creation": time.time(),
+    "last_accessed": time.time(),
+    "access_count": 0,
+    "last_folder": "",
+    "last_file": ""
+}
+
+
 class DataBase:
     __currentFolder = ""
     __currentFile = ""
+
     def __init__(self, source: str) -> None:
         self.source = source
 
         if not os.path.exists(source) or len(os.listdir(source)) <= 0:
             self.__create_db()
-        else:
-            self.__open_db()
 
     def __create_db(self):
-        return
+        try:
+            os.mkdir(self.source)
+        except FileExistsError:
+            pass
 
-    def gen_conf(self):  # config file spec can be found in documentation
-        return
+        with open(f"{self.source}db_config.json", "w") as c:
+            json.dump(EMPTY_CONFIG, c, indent=3)
 
-    def __open_db(self):
+    def open(self):
         start = time.time()
         try:
-            with open(f"{self.source}db_config.json", "r") as c_file:
-                config = json.loads(c_file.read())
+            with open(f"{self.source}db_config.json", "r+") as c:
+                config = json.load(c)
+                c.truncate(0)
+
+            config["last_accessed"] = time.time()
+            config["access_count"] += 1
+            self.__currentFolder = config["last_folder"]
+            self.__currentFile = config["last_file"]
+
+            with open(f"{self.source}db_config.json", "r+") as c:
+                json.dump(config, c, indent=3)
+
         except (FileNotFoundError, json.JSONDecodeError) as e:
             raise DBError(f"cannot load config: {e}")
-
 
         end = time.time()
         return
 
-#DataBase("D:\\CompSciNEA\\meteo_brain\\tests\\example_db\\")
 
-
-
-
+DataBase("D:\\CompSciNEA\\meteo_brain\\tests\\test_db\\").open()
