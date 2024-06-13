f9b7a23edc5f9d7e00a05f685d0c8be617350e60
Alfred G.L Taylor <alfredgtaylor@gmail.com>
2024-06-13 14:53:23 +0100

    still working on db.py

--- src/meteo_brain/data/db.py
+++ src/meteo_brain/data/db.py
@@ -17,7 +17,7 @@
 import os
 import time
 import json
-import numpy
+import pandas
 
 
 class DBError(Exception):  # custom exception class to make error checking easier
@@ -43,37 +43,36 @@ class DataBase:
         if not os.path.exists(source) or len(os.listdir(source)) <= 0:
             self.__db_init()
 
-    def __repr__(self):
-        return
-
     def __db_init(self):  # create conf file without adding any subfolders yet
         try:
             os.mkdir(self.source)
         except FileExistsError:
             pass
 
-        with open(f"{self.source}db_config.json", "w") as c:
+        with open(f"{self.source}/db_config.json", "w") as c:
             json.dump(EMPTY_CONFIG, c, indent=3)
 
-    def __fs_init(self):  # start database file struct
+    def __fs_init(self) -> None:  # start database file struct
         year = time.strftime("%Y")
         month = time.strftime("%b").lower()  # string month names for easy reading
 
         path = f"{self.source}/{year}/{month}/"  # potentially add folders for day of month too?
         os.makedirs(path)
 
-        fname = time.strftime("%H-%M-%S") + ".csv"
-        open(f"{path}{fname}", "a").close()  # why cant python have a nicer looking way to make files
+        f_name = time.strftime("%a-%d-%H") + ".csv"
+        open(f"{path}{f_name}", "a").close()  # why cant python have a nicer looking way to make files
 
         config = self.get_conf()
         config["last_folder"] = path
-        config["last_file"] = fname
+        config["last_file"] = f_name
         self.set_conf(config)
 
     def get_conf(self) -> dict:  # getter function for the config file
         try:
             with open(f"{self.source}/db_config.json", "r+") as c:
                 config = json.load(c)
+                self.__currentFolder = config["last_folder"]  # setting the globals (I hate globals)
+                self.__currentFile = config["last_file"]
                 return config
 
         except (FileNotFoundError, json.JSONDecodeError) as e:
@@ -82,27 +81,33 @@ class DataBase:
     def set_conf(self, content: dict) -> None:  # setter function for the config file
         try:
             with open(f"{self.source}/db_config.json", "w+") as c:
+                self.__currentFolder = content["last_folder"]  # setting the globals (I hate globals)
+                self.__currentFile = content["last_file"]
                 c.truncate(0)
                 json.dump(content, c, indent=3)
 
         except (FileNotFoundError, json.JSONDecodeError) as e:
             raise DBError(f"cannot load config: {e}")
 
-    def open(self) -> float:  # function returns time taken to open db in ms
+    def open(self) -> None:  # prepare db for use
         start = time.time()
 
         config = self.get_conf()
         config["last_accessed"] = time.time()
         config["access_count"] += 1
-        self.__currentFolder = config["last_folder"]
-        self.__currentFile = config["last_file"]
         self.set_conf(config)
 
-        if self.__currentFolder == "":  # only should run after __db_init
+        if self.__currentFolder == "" or self.__currentFile == "":  # only should run after __db_init
             self.__fs_init()
 
-        end = time.time()
-        return (end - start) * 1000
+    def fetch(self) -> pandas.DataFrame:
+        try:
+            d = pandas.read_csv(f"{self.__currentFolder}{self.__currentFile}", sep=",", header=0)
+        except pandas.errors.EmptyDataError as e:
+            raise DBError(f"database file {self.__currentFile} invalid: {e}")
+        return d
+    def send(self, datavals):
+        timestamp = time.time()
 
+        return
 
-DataBase("D:\\CompSciNEA\\meteo_brain\\tests\\test_db\\").open()
