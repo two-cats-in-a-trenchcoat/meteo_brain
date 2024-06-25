e8c49791a676508b827a63c33b047cc950f0ac50
Alfred G.L Taylor <alfredgtaylor@gmail.com>
2024-06-25 11:13:38 +0100

    still working on db.py
    Signed-off-by: Alfred G.L Taylor <alfredgtaylor@gmail.com>

--- src/meteo_brain/data/db.py
+++ src/meteo_brain/data/db.py
@@ -14,20 +14,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# python stdlib moment
 import json
 import os
 import time
 import webbrowser
 from typing import *
 
-import pandas
+# pd is by itself because it is a naughty dependency that takes ages to initialise
+import pandas as pd
 
 
 class DBError(Exception):  # custom exception class to make error checking easier
     pass
 
 
-EMPTY_CONFIG = {  # maybe this should be a local variable
+EMPTY_CONFIG: dict = {  # maybe this should be within DataBase's scope
     "date_of_creation": time.time(),
     "last_accessed": time.time(),
     "access_count": 0,
@@ -35,7 +37,7 @@ EMPTY_CONFIG = {  # maybe this should be a local variable
     "last_file": ""
 }
 
-SECONDS_IN_A_DAY = 86400
+SECONDS_IN_A_DAY: int = 86400
 
 
 class DataBase:
@@ -44,7 +46,7 @@ class DataBase:
 
     def __init__(self, source: str, sensor_labels: list) -> None:  # database checks it exists on init
         self.source = source
-        self.edf = pandas.DataFrame(columns=sensor_labels)
+        self.empty_data_frame = pd.DataFrame(columns=sensor_labels)
         self.labels = sensor_labels
 
         if not os.path.exists(f"{source}/db_config.json") or len(os.listdir(source)) <= 0:
@@ -58,7 +60,7 @@ class DataBase:
         if self.__currentFolder == "" or self.__currentFile == "":  # only should run after __db_init has been run once
             self.__fs_init()
 
-    def __db_init(self):  # create conf file without adding any subfolders yet
+    def __db_init(self) -> None:  # create conf file without adding any subfolders yet
         try:
             os.mkdir(self.source)
         except FileExistsError:
@@ -86,7 +88,7 @@ class DataBase:
         config["last_file"] = f_name
         self.set_conf(config)
 
-    def __fs_check(self) -> None:
+    def __fs_check(self) -> None:  #
         self.get_conf()
         year = time.strftime("%Y")
         month = time.strftime("%b").lower()
@@ -116,11 +118,11 @@ class DataBase:
     def __gen_file(self) -> str:  # generate files as time changes
         path = self.__currentFolder
         f_name = f"{int(time.time())}-{time.strftime('%a-%d')}.csv"
-        self.edf.to_csv(f"{path}{f_name}", sep=",", index=True)
+        self.empty_data_frame.to_csv(f"{path}{f_name}", sep=",", index=True)
         return f_name
 
     def get_conf(self) -> dict:  # getter function for the config file
-        self.__fs_check()  # best practice call __fs_check before directly manipulating the database
+        # __fs_check calls get_conf, so it cannot be used here.
 
         try:
             with open(f"{self.source}/db_config.json", "r+") as c:
@@ -137,55 +139,53 @@ class DataBase:
 
         try:
             with open(f"{self.source}/db_config.json", "w+") as c:
+                # reformat path to be POSIX compliant
                 content["last_folder"] = content["last_folder"].replace("\\", "/").replace("//", "/")
                 self.__currentFolder = content["last_folder"]  # update private variables
                 self.__currentFile = content["last_file"]
-                c.truncate(0)
+                c.truncate(0)  # clear file
                 json.dump(content, c, indent=3)
 
         except (FileNotFoundError, json.JSONDecodeError) as e:
             raise DBError(f"cannot load config: {e}")
 
-    # with large file sizes running this function often is ill-advised
-    def fetch_current(self, filepath=None) -> pandas.DataFrame:
+    # with large file sizes running this function often is ill-advised, it eats ram. Perhaps cache values before commit?
+    def fetch_current(self, filepath=None) -> pd.DataFrame:
         self.__fs_check()  # best practice call __fs_check before directly manipulating the database
 
-        if filepath is None:
+        if filepath is None:  # cannot accept self as a default argument so this check has to be made
             filepath = f"{self.__currentFolder}{self.__currentFile}"
 
         try:
-            d = pandas.read_csv(filepath, sep=",", header=0, index_col=0)
+            data = pd.read_csv(filepath, sep=",", header=0, index_col=0)
 
-        except pandas.errors.EmptyDataError as e:
+        except (pd.errors.EmptyDataError, FileNotFoundError) as e:
             raise DBError(f"database file {self.__currentFile} invalid: {e}")
 
-        except FileNotFoundError:
-            raise DBError(f"database file {self.__currentFile} was deleted during runtime")
+        return data
 
-        return d
-
-    def fetch_historic(self, day_month_year: Tuple[int, int, int]) -> Optional[pandas.DataFrame]:
+    # this func is the same as fetch() but slower...
+    def fetch_historic(self, day_month_year: Tuple[int, int, int]) -> Optional[pd.DataFrame]:
         self.__fs_check()  # best practice call __fs_check before directly manipulating the database
 
+        # non-discerning search algorithm, will search every file in database, room for speed improvements here
         for root, dirs, files in os.walk(self.source):
             file_split = [x.split("-")[0] for x in files]
 
-            for i in range(len(file_split)):
+            for i, j in enumerate(file_split):
                 try:
-                    int(file_split[i])
-
-                    if time.localtime(int(file_split[i])).tm_mday == day_month_year[0]:
-                        if time.localtime(int(file_split[i])).tm_mon == day_month_year[1]:
-                            if time.localtime(int(file_split[i])).tm_year == day_month_year[2]:
-                                return self.fetch_current(os.path.join(root, files[i]))
-                except ValueError:
+                    # see if there is a better way to do this
+                    if (time.localtime(int(j)).tm_mday == day_month_year[0] and
+                            time.localtime(int(j)).tm_mon == day_month_year[1] and
+                            time.localtime(int(j)).tm_year == day_month_year[2]):
+                        return self.fetch_current(os.path.join(root, files[i]))
+                except ValueError:  # this is also scuffed as fuck
                     pass
 
-        return None
-
-    def commit_frame(self, data: pandas.DataFrame) -> None:  # append multiple entries in the form of a dataframe
+    def commit_frame(self, data: pd.DataFrame) -> None:  # append multiple entries in the form of a dataframe
         self.__fs_check()  # best practice call __fs_check before directly manipulating the database
 
+        # load file as DataFrame, modify dataframe, wipe file and then re-write.
         to_append = self.fetch_current()
 
         if data.columns.empty:
@@ -193,28 +193,25 @@ class DataBase:
 
         open(f"{self.__currentFolder}{self.__currentFile}", "w").close()  # why, python, WHY!!!
 
+        # merge the two DataFrames along their columns, if both share indexes this may cause issues
         if not to_append.empty:
-            data = pandas.concat([to_append, data])
+            data = pd.concat([to_append, data])
 
         data.to_csv(f"{self.__currentFolder}{self.__currentFile}", mode="w")
 
-        return
-
-    def display(self, dataset: pandas.DataFrame) -> None:  # display current db file as html
+    def display(self, dataset: pd.DataFrame) -> None:  # display current db file as html
         url = f"{self.source}/tmp.html"
         dataset.to_html(url)
         webbrowser.open(url)
-        return
+        os.remove(url)
 
-    def commit_item(self, data_item: List[int]) -> None:  # add only one line of data
+    def commit_item(self, data_item: List[int]) -> None:  # add only one line of data to current database file
         self.__fs_check()  # best practice call __fs_check before directly manipulating the database
 
+        # load file as DataFrame, modify dataframe, wipe file and then re-write.
         data = self.fetch_current()
         data.loc[len(data)] = data_item
 
         open(f"{self.__currentFolder}{self.__currentFile}", "w").close()  # why, python, WHY!!!
 
         data.to_csv(f"{self.__currentFolder}{self.__currentFile}", mode="w")
-        return
-
-
